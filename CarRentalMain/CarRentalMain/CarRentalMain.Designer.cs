161c5a99ee8fba9c5d42b0c46e416acf3bbb1860
Caratred <dassowb0941@my.uwstout.edu>
2016-10-06 14:36:16 -0500

    First Commit

---
+++ CarRentalMain/CarRentalMain/CarRentalMain.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace CarRentalMain
+{
+    partial class CarRentalMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.addTextBox = new System.Windows.Forms.TextBox();
+            this.mainRichText = new System.Windows.Forms.RichTextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(122, 3);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 0;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.exitButton.Location = new System.Drawing.Point(353, 316);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 1;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.addTextBox);
+            this.panel1.Controls.Add(this.addButton);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 31);
+            this.panel1.TabIndex = 2;
+            // 
+            // addTextBox
+            // 
+            this.addTextBox.Location = new System.Drawing.Point(3, 5);
+            this.addTextBox.Name = "addTextBox";
+            this.addTextBox.Size = new System.Drawing.Size(113, 20);
+            this.addTextBox.TabIndex = 1;
+            // 
+            // mainRichText
+            // 
+            this.mainRichText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mainRichText.Location = new System.Drawing.Point(15, 49);
+            this.mainRichText.Name = "mainRichText";
+            this.mainRichText.ReadOnly = true;
+            this.mainRichText.Size = new System.Drawing.Size(413, 261);
+            this.mainRichText.TabIndex = 3;
+            this.mainRichText.Text = "";
+            // 
+            // CarRentalMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 351);
+            this.Controls.Add(this.mainRichText);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.exitButton);
+            this.Name = "CarRentalMain";
+            this.Text = "Car Rental";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox addTextBox;
+        private System.Windows.Forms.RichTextBox mainRichText;
+    }
+}
+
---
+++ CarRentalMain/CarRentalMain/CarRentalMain.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarRentalMain
+{
+    public partial class CarRentalMain : Form
+    {
+        public CarRentalMain()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("What car would you like to add?");
+            mainRichText.Text += addTextBox.Text + "\n";
+            addTextBox.Text = "";
+            addTextBox.Focus();// = true;
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+    }
+}
